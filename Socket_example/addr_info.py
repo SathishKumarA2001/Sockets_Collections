b60da5409c46e43aeaec408fa7c478339466fad3
Sathish Kumar <chesathish5201@gmail.com>
2021-06-11 14:46:16 +0530

    Updated

--- Socket_example/addr_info.py
+++ Socket_example/addr_info.py
@@ -6,7 +6,46 @@ import socket
 #for i in addr_info:
 #    print('\n',i)
 
-addr_info = socket.getaddrinfo("Example.com",80,family=socket.AF_INET,proto=socket.IPPROTO_TCP)
-print(addr_info)
-addr_info = socket.getaddrinfo("Example.com",80,family=socket.AF_INET6,proto=socket.IPPROTO_TCP)
-print(addr_info)
+#addr_info = socket.getaddrinfo("Example.com",80,family=socket.AF_INET,proto=socket.IPPROTO_TCP)
+#print(addr_info)
+#addr_info = socket.getaddrinfo("Example.com",80,family=socket.AF_INET6,proto=socket.IPPROTO_TCP)
+#print(addr_info)
+
+#host = socket.gethostbyaddr("192.0.43.9")    #gethostbyaddr
+#print("The host name of IP is : ",host)
+
+#IP = socket.gethostbyname("selfmade.ninja")    #gethostbyname
+#print("The host name of IP is : ",IP)
+
+#ex = socket.gethostbyname_ex("selfmade.ninja")    #gethostbyname_ex
+#print("The host name of IP is : ",ex)
+
+#host = socket.gethostname()                       #gethostname
+#print("The host name of IP is : ",host)
+#Myhost = socket.gethostbyname(host)
+#print("The host name of IP is : ",Myhost)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
